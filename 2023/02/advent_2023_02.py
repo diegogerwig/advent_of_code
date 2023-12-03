d5064a027ff3e94f1edd02c82a9845a924864287
diegogerwig <diegogerwig@gmail.com>
2023-12-03 22:16:57 +0100

    2023_12_03..22:16:57

--- 2023/02/advent_2023_02.py
+++ 2023/02/advent_2023_02.py
@@ -17,26 +17,26 @@ with open("inp.txt") as f:
 #########################
 
 answer_a = 0
-bads = set()
+game_wrong = set()
 
 for line in lines:
-    gn, rounds = line.split(':')
-    gid = int(gn[5:])
+    game_num, rounds = line.split(':')
+    game_id = int(game_num[5:])
 
-    answer_a += gid
+    answer_a += game_id
 
     rounds = rounds.split(';')
     for round in rounds:
         round = [r.strip().split() for r in round.split(',')]
         for k, color in round:
             if color == 'red' and int(k) > 12:
-                bads.add(gid)
+                game_wrong.add(game_id)
             if color == 'green' and int(k) > 13:
-                bads.add(gid)
+                game_wrong.add(game_id)
             if color == 'blue' and int(k) > 14:
-                bads.add(gid)
+                game_wrong.add(game_id)
 
-print('ANSWER Part A -> ', answer_a - sum(bads))
+print('ANSWER Part A -> ', answer_a - sum(game_wrong))
 
 
 #########################
@@ -44,9 +44,10 @@ print('ANSWER Part A -> ', answer_a - sum(bads))
 #########################
 
 answer_b = 0
+
 for line in lines:
-    gn, rounds = line.split(':')
-    gid = int(gn[5:])
+    game_num, rounds = line.split(':')
+    game_id = int(game_num[5:])
 
     rounds = rounds.split(';')
 
--- 2023/03/advent_2023_03.py
+++ 2023/03/advent_2023_03.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+from string import digits
 
 # with open("inp_test_a.txt") as f:
 #     lines = f.read().split('\n')
@@ -15,15 +17,98 @@ with open("inp.txt") as f:
 ###      PART A      ####
 #########################
 
+answer_a = 0
+sd = defaultdict(bool)
+symbols = {'#', '$', '%', '&', '*', '+', '-', '/', '=', '@'}
 
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c in symbols:
+            sd[(i, j)] = True
 
-print('ANSWER Part A -> ', )
+seen = set()
+k = ''
+pos = []
+
+def _check_adjacencies():
+    adjs = [
+        (-1, -1), (0, -1), (1, -1),
+        (-1, 0), (1, 0),
+        (-1, 1), (0, 1), (1, 1)
+    ]
+    for x, y in pos:
+        for a, b in adjs:
+            if (x + a, y + b) in sd:
+                return int(k)
+
+    return 0
+
+for i, line in enumerate(lines):
+    if k:
+        answer_a += _check_adjacencies()
+        k = ''
+        pos = []
+    for j, c in enumerate(line):
+        if c in symbols or c == '.':
+            answer_a += _check_adjacencies()
+            k = ''
+            pos = []
+        if c in digits:
+            k += c
+            pos.append((i, j))
+
+print('ANSWER Part A -> ', answer_a)
 
 
 #########################
 ###      PART B      ####
 #########################
 
+answer_b = 0
+sd = defaultdict(bool)
+gears = []
+numbers = []
 
+for i, line in enumerate(lines):
+    for j, c in enumerate(line):
+        if c == '*':
+            gears.append((i, j))
 
-print('ANSWER Part B -> ', )
+seen = set()
+k = ''
+pos = []
+
+for i, line in enumerate(lines):
+    if k:
+        numbers.append((k, list(pos)))
+        k = ''
+        pos = []
+    for j, c in enumerate(line):
+        if k and c not in digits:
+            numbers.append((k, list(pos)))
+            k = ''
+            pos = []
+        if c in digits:
+            k += c
+            pos.append((i, j))
+
+def _check_adjacencies(xg, yg):
+    tmp = []
+    adjs = [
+        (-1, -1), (0, -1), (1, -1),
+        (-1, 0), (1, 0),
+        (-1, 1), (0, 1), (1, 1)
+    ]
+    for number, positions in numbers:
+        for a, b in adjs:
+            if (xg + a, yg + b) in positions:
+                tmp.append(int(number))
+                break
+    return tmp
+
+for gear in gears:
+    x = _check_adjacencies(*gear)
+    if len(x) == 2:
+        answer_b += (x[0] * x[1])
+
+print('ANSWER Part B -> ', answer_b)
