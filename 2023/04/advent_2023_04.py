71efb16e25b38ee8c71b4080541aa4e1a84fb2a5
Diego Gerwig <dgerwig-@student.42urduliz.com>
2023-12-04 16:36:00 +0100

    2023_12_04%..16:36:00

---
+++ 2023/04/advent_2023_04.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from queue import Queue
+
+# with open("inp_test_a.txt") as f:
+#     lines = f.read().split('\n')
+
+# with open("inp_test_b.txt") as f:
+#     lines = f.read().split('\n')
+
+with open("inp.txt") as f:
+    lines = f.read().split('\n')
+
+
+#########################
+###      PART A      ####
+#########################
+
+answer_a = 0
+
+for line in lines:
+    _, numbers = line.split(':')
+    winner, ours = numbers.split('|')
+    ws = set(winner.split())
+    os = set(ours.split())
+    matches = ws & os
+    if matches:
+        answer_a += 2 ** (len(matches) - 1)
+
+print('ANSWER Part A -> ', answer_a)
+
+
+#########################
+###      PART B      ####
+#########################
+
+answer_b = 0
+d = {}
+q = Queue()
+
+for line in lines:
+    cid, numbers = line.split(':')
+    cid = int(cid[4:])
+    winner, ours = numbers.split('|')
+    ws = set(winner.split())
+    os = set(ours.split())
+    matches = ws & os
+    d[cid] = len(matches)
+    q.put(cid)
+
+while not q.empty():
+    answer_b += 1
+    k = q.get()
+    for i in range(k + 1, k + d[k] + 1):
+        q.put(i)
+
+print('ANSWER Part B -> ', answer_b)
